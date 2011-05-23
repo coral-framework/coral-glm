a7d6d4b1eae0c505cf04353e6e523f7b64dcb05d
xvaldetaro <xvaldetaro@gmail.com>
2011-05-23 17:31:30 -0300

    Mat4 functions and tests, overloads nyi

--- modules/glm/__init.lua
+++ modules/glm/__init.lua
@@ -11,7 +11,7 @@ function M.vec3( x, y, z )
 	return out
 end
 
-function M.matrix()
+function M.mat4()
 	return co.new "glm.Mat4"
 end
 
@@ -24,7 +24,10 @@ end
 -- All the functions mimics GLSL API. Except for   --
 -- an optional "out" parameter that be used as     --
 -- return value for efficiency. All functions      --
--- create and return a new type if "out" is nil    --
+-- create and return a new type if "out" is nil.   --
+-- Do not pass the same reference to a function`s  --
+-- second parameter and out. eg: 				   --	
+-- local v = glm.vec3(); glm.cross( otherV, v, v ) --
 -----------------------------------------------------
 --------------------------------------------
 --           Vec3 functions               --
@@ -58,6 +61,15 @@ function M.normalize( v, out )
 	return out	
 end
 
+-- returns the Vec3 result of ( Mat4 m * Vec3 v )
+function M.transform( v, m, out )
+	out = out or co.new "glm.Vec3"
+	
+	out:copy( v )
+	out:transform( m )
+	return out	 
+end
+
 --------- Multiple Getters/Setters ---------
 
 -- returns the float components of Vec3 v 
@@ -70,10 +82,9 @@ function M.setXYZ( v, x, y, z )
 	v:setXYZ( x, y, z )
 end
 
-
 ------------- Basic Operations -----------
 
--- returns the Vec3 result of the Vec3s v1 + v2
+-- returns the Vec3 result of the Vec3 v1 + Vec3 v2
 function M.addVec( v1, v2, out )
 	out = out or co.new "glm.Vec3"
 	
@@ -113,6 +124,20 @@ end
 --           Mat4 functions               --
 --------------------------------------------
 
+-- returns m[i][j]
+function M.getElement( m, i, j )
+	return m:getElement( i, j )
+end
+
+-------- Basic Algebric operations ---------
+
+-- returns the identity matrix
+function M.identity( out ) 
+	out = out or co.new "glm.Mat4"
+	out:identity()
+	return out
+end
+	
 -- returns the Mat4 inserve of m
 function M.inverse( m, out )
 	out = out or co.new "glm.Mat4"
@@ -127,16 +152,113 @@ function M.transpose( m, out )
 	out = out or co.new "glm.Mat4"
 	
 	out:copy( m )
-	out:invert()
+	out:transpose()
 	return out	
 end
 
+-- rotates a copy of m of Scalar degrees around Vec3 axis and returns it
+function M.rotate( m, degrees, axis, out )
+	out = out or co.new "glm.Mat4"
+	
+	out:copy( m )
+	out:rotate( degrees, axis )
+	return out	
+end
 
+-- scales a copy of m by the amount on each axis of Vec3 scale and returns it
+function M.scale( m, scale, out )
+	out = out or co.new "glm.Mat4"
+	
+	out:copy( m )
+	out:scale( scale )
+	return out	
+end
+
+-- translates a copy of m by Vec3 position and returns it
+function M.translate( m, position, out )
+	out = out or co.new "glm.Mat4"
+	
+	out:copy( m )
+	out:translate( position )
+	return out	
+end
+
+-- returns the mat4 result of mat4 m1 * m2
+function M.mul( m1, m2, out )
+	out = out or co.new "glm.Mat4"
+	out:copy( m1 )
+	out:preMul( m2 )
+	return out
+end
+
+-- returns the mat4 result of mat4 m * double s
+function M.mulScalar( m, s, out )
+	out = out or co.new "glm.Mat4"
+	out:copy( m )
+	out:mulScalar( s )
+	return out
+end
+
+------------- Camera Operations ------------
+
+-- parameters: vec3 eye, vec3 center and vec3 up
+function M.lookAt( eye, center, up, out )
+	out = out or co.new "glm.Mat4"
+	out:lookAt( eye, center, up )
+	return out
+end
+
+-- parameters: double left, right, bottom, top
+function M.ortho( left, right, bottom, top, out )
+	out = out or co.new "glm.Mat4"
+	out:ortho( left, right, bottom, top )
+	return out
+end
+
+-- parameters: double fovy, aspect, zNear, zFar
+function M.perspective( fovy, aspect, zNear, zFar, out )
+	out = out or co.new "glm.Mat4"
+	out:perspective( fovy, aspect, zNear, zFar )
+	return out
+end
+
+-- parameters: double left, right, bottom, top, nearVal, farVal
+function M.frustum( left, right, bottom, top, nearVal, farVal, out )
+	out = out or co.new "glm.Mat4"
+	out:frustum( left, right, bottom, top, nearVal, farVal )
+	return out
+end
+
+--------------------------------------------
+--          Specific operators            --
+--------------------------------------------
+-- function for overloading the * operator for vec3 (tests which parameter is a scalar)
+function M.vecScalarMulOperator( a, b )
+	if type( b ) == "number" then
+		return M.mulVec( a, b )
+	end
+	return M.mulVec( b, a )
+end
+
+-- function for overloading the * operator for vec3 (tests which parameter is a scalar)
+function M.vecScalarDivOperator( a, b )
+	if type( b ) == "number" then
+		return M.divVec( a, b )
+	end
+	return M.divVec( b, a )
+end
+
+-- operators for mat4
+M.idMat = co.new "glm.Mat4"
+local matMT = getmetatable( M.idMat )
+matMT.__mul = M.mul
+
+-- operators for vec3
 M.zeroVec = co.new "glm.Vec3"
 local vecMT = getmetatable( M.zeroVec )
 vecMT.__add = M.addVec
 vecMT.__sub = M.subVec
-vecMT.__div = M.divVec
-vecMT.__mul = M.mulVec
+vecMT.__div = M.vecScalarDivOperator
+vecMT.__mul = M.vecScalarMulOperator
 
 return M
--- tests/MatrixTests.lua
+++ tests/MatrixTests.lua
@@ -10,3 +10,78 @@ local glm = require "glm"
 -- Matrix Tests
 -----------------------------
 
+local m1
+local m2
+local m3
+local m4
+
+function elementIdRST()
+	m1 = glm.mat4()
+	m2 = glm.mat4()
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 0, 0 ), 1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 1, 1 ), 1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 2, 0 ), 0 )
+	
+	glm.rotate( m1, 90, glm.vec3( 0, 1, 0 ),  m1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 0, 0 ), 0 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 0, 2 ), -1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 2, 0 ), 1 )
+	
+	glm.scale( m2, glm.vec3( 2, 2, 2 ), m2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m2, 0, 0 ), 2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m2, 1, 1 ), 2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m2, 2, 2 ), 2 )
+	
+	glm.identity( m1 )
+	glm.translate( m1, glm.vec3( 2, 2, 2 ), m1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 0 ), 2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 1 ), 2	)
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 2 ), 2 )
+end
+
+function invTranpTest()
+	m1 = glm.mat4()
+	
+	glm.translate( m1, glm.vec3( 2, 2, 2 ), m1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 0 ), 2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 1 ), 2	)
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 2 ), 2 )
+	
+	glm.inverse( m1, m1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 0 ), -2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 1 ), -2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 2 ), -2 )
+	
+	glm.transpose( m1, m1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 0, 3 ), -2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 1, 3 ), -2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 2, 3 ), -2 )
+end
+
+function cameraTest()
+	m1 = glm.lookAt( glm.vec3( 0, 0, -1 ), glm.vec3( 0, 0, 1 ), glm.vec3( 0, 1, 0 ) )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 0, 0 ), -1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 2, 2 ), -1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 2 ), -1 )
+	
+	m2 = glm.frustum( -1, 1, -1, 1, 2, 4 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m2, 0, 0 ), 2 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m2, 2, 2 ), -3 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m2, 3, 2 ), -8 )
+	
+	m3 = glm.ortho( -1, 1, -1, 1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m3, 0, 0 ), 1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m3, 2, 2 ), -1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m3, 3, 2 ), 0 )
+	
+	m4 = glm.perspective( 3.1415 / 2, 1.0, 1.0, 10.0 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m4, 0, 0 ), 72.948834620885 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m4, 2, 2 ), -11/9 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m4, 2, 3 ), -1 )
+	
+	m1 = m3 * m1
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 0, 0 ), -1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 2, 2 ), 1 )
+	ASSERT_DOUBLE_EQ( glm.getElement( m1, 3, 2 ), 1 )
+end
+
